36d6350b6e14f5193e625bad50f3d049a1fcbebc
Elsie Puig <epuigsant@gmail.com>
2016-11-07 10:18:36 -0800

    style header

--- header.php
+++ header.php
@@ -11,29 +11,39 @@
 	
 	<!---HEADER--->
 	
-	<div class="header">
-		
-		<header>
-			<div class="blog-site-title">
-				<h1><a href="<?php $url = home_url('/'); echo $url; ?>"><?php bloginfo('name'); ?></a></h1>
-				<h2><?php bloginfo('description'); ?></h2>
-			</div>
-		</header>
+	<header class="header">
+		<div class="headerimg">
 			
-			<div class="site-menu">
+			<!--SITE MENU-->
+			
+			<div class="top-level-menu">
+				
+				<h1><a href="<?php $url = home_url('/'); echo $url; ?>"><?php bloginfo('name'); ?></a></h1>
+				
+				<div class="site-menu">
 				<?php
                     wp_nav_menu( array(
                     'sort_column' => 'menu_order',
                     'container_class' => 'blank-menu-header'
 				));
 				?>
+				</div>
+			
 			</div>
-		<div class="blog-intro">
-			<h2>Web Interactivity and Engagement</h2>
-			<h3>Improving User Experience Through Wordpress</h3>
-			<p>Welcome. In this blog you'll learn about building user-friendly websites using Wordpress. Expect timely content on content management, information governance, user experience, and web development.</p>
+			
+			
+			<!--SITE NAME AND DESCRIPTION -->
+			
+			<div class="transparent-background">
+				<div class="blog-site-title">
+				<h1><?php bloginfo( 'name' ); ?></h1>
+				<h2><?php bloginfo('description'); ?></h2>
+				<p>A blog covering the breadth and depth of building websites using Wordpress -- including server-side programming, content management, user experience, and content marketing.</p>
+			</div>
+			</div>
 		</div>
-	</div>
+	</header>
+	
 	
 	<!---PROFILE--->
 	
@@ -41,7 +51,7 @@
 		<div class="profile-image">
 		</div>
 		<div class="brief-bio">
-			<h3>Behind The Blog</h3>
+			<h3>Behind The Blog: Elsie Puig</h3>
 			<p>Elsie Puig is a writer, web designer, intern, mom, wife, and student living in Richland, WA. Her passion is brinding the gap between useful content and great design.</p>
 		</div>
 	</div>
